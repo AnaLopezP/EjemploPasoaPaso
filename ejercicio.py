7a02e6cc8d8a270db92c0ca0bba6b1ae9c91562a
Ana López <anamuyamiga@gmail.com>
2023-03-13 15:47:51 +0100

    importsss

---
+++ ejercicio.py
@@ -0,0 +1,13 @@
+#Importamos todas las librerías necesarias
+from urllib.parse import *
+from contextlib import closing
+from http.client import *
+from sys import *
+from bs4 import *
+from os import sep
+from timeit import *
+import asyncio
+from aiohttp import *
+from functools import partial
+import html.parser
+
